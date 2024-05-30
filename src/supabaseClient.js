7cb9d45c01ed21bb716477e3cf2ade0e1b6ab53c
siddharth <rathoresiddharth1905@gmail.com>
2024-05-30 18:28:21 +0530

    third commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mpqxgjnwnkdyynydgaxe.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wcXhnam53bmtkeXlueWRnYXhlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNjk5MDkxMiwiZXhwIjoyMDMyNTY2OTEyfQ.xaceUA4bWT0wWtVoBFNiOyYHAvQfL24VsG5Wk1ZriAA';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
